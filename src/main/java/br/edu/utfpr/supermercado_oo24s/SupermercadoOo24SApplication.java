dd7a7387f0679ebd8ea22d230999b08f73fc8e89
Marco Antonio Pegoraro <marco.antonio.pegoraro1@gmail.com>
2022-04-22 14:10:25 -0300

    first commit

---
+++ src/main/java/br/edu/utfpr/supermercado_oo24s/SupermercadoOo24SApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.supermercado_oo24s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermercadoOo24SApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermercadoOo24SApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/utfpr/supermercado_oo24s/SupermercadoOo24SApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.supermercado_oo24s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermercadoOo24SApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

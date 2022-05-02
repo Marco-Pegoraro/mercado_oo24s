89da022387938618a2b4c6101100337ddf0e048d
Marco Antonio Pegoraro <marco.antonio.pegoraro1@gmail.com>
2022-05-02 16:52:22 -0300

    adicionando model do produto

---
+++ src/main/java/br/edu/utfpr/supermercado_oo24s/model/Produto.java
@@ -0,0 +1,27 @@
+package br.edu.utfpr.supermercado_oo24s.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer id;
+
+    public String nome;
+    public String descricao;
+    public Double valor;
+    public Integer quantidade;
+}

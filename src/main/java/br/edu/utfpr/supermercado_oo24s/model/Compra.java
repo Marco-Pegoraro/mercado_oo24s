5029378ebe189eb639596ef5267826f73d96d924
Marco Antonio Pegoraro <marco.antonio.pegoraro1@gmail.com>
2022-05-06 19:39:37 -0300

    inserção variaveis do Compra

--- src/main/java/br/edu/utfpr/supermercado_oo24s/model/Compra.java
+++ src/main/java/br/edu/utfpr/supermercado_oo24s/model/Compra.java
@@ -1,4 +1,17 @@
 package br.edu.utfpr.supermercado_oo24s.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer id;
+
+    public String cpf;
+    public Double totalCompra;
 }

5f8505bf31b7383a6024b41a49e36d2de6dfec2a
Marco Antonio Pegoraro <marco.antonio.pegoraro1@gmail.com>
2022-05-13 19:43:07 -0300

    inserção das funções de validar e imprimir CPF

--- src/main/java/br/edu/utfpr/supermercado_oo24s/SupermercadoOo24SApplication.java
+++ src/main/java/br/edu/utfpr/supermercado_oo24s/SupermercadoOo24SApplication.java
@@ -1,13 +1,21 @@
 package br.edu.utfpr.supermercado_oo24s;
 
+import br.edu.utfpr.supermercado_oo24s.service.ValidadorCPF;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SupermercadoOo24SApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SupermercadoOo24SApplication.class, args);
-    }
+    private ValidadorCPF validadorCPF;
 
+    public static void main(String[] args) {SpringApplication.run(SupermercadoOo24SApplication.class, args);}
+
+    /*
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        System.out.println(validadorCPF.isCPF("60014194007"));
+        System.out.println(validadorCPF.imprimeCPF("60014194007"));
+    }
+    */
 }

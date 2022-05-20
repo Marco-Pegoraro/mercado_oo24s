494da32cda5646a0e2fb7d6cad40c2b486e0eead
Marco Antonio Pegoraro <marco.antonio.pegoraro1@gmail.com>
2022-05-20 19:12:21 -0300

    criação EstoqueService e ProdutoService

--- src/main/java/br/edu/utfpr/supermercado_oo24s/SupermercadoOo24SApplication.java
+++ src/main/java/br/edu/utfpr/supermercado_oo24s/SupermercadoOo24SApplication.java
@@ -1,21 +1,24 @@
 package br.edu.utfpr.supermercado_oo24s;
 
 import br.edu.utfpr.supermercado_oo24s.service.ValidadorCPF;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SupermercadoOo24SApplication {
+
+    @Autowired
+
 
     private ValidadorCPF validadorCPF;
 
     public static void main(String[] args) {SpringApplication.run(SupermercadoOo24SApplication.class, args);}
 
-    /*
     @EventListener(ApplicationReadyEvent.class)
     public void doSomethingAfterStartup() {
-        System.out.println(validadorCPF.isCPF("60014194007"));
-        System.out.println(validadorCPF.imprimeCPF("60014194007"));
+
     }
-    */
 }
